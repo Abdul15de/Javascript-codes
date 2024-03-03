0eac12ad20e87fc65f26835bc0136383a624f30d
Abdul15de <126737630+Abdul15de@users.noreply.github.com>
2024-03-03 05:22:05 +0000

    tricky conversions

---
+++ .devcontainer/comparsion.js
@@ -0,0 +1,24 @@
+/*
+let str = "hi";
+let str2 = "Javascript";
+console.log(str + " " + str2); //normal conversion
+*/
+
+
+// *****************TRICKY CONVERSIONS **********
+/*
+console.log("1" + 1) //  11
+console.log(1 + "1") //  11
+console.log("1" + 1 + 1) //  111 BECAUSE js evaluates from left to right and jo data type pahle ata hai usko pahle leta hai here string comes first so srting is taken
+console.log(1 + 1 + "1") // 21 because it evaluates first 1+1 which gives 2 then 2+"1" wehich gives 21  
+*/
+
+
+/*
+console.log(+true)// 1
+console.log(true+)// error
+console.log(+""); //0
+*/
+
+
+//  NOTE!!! these all above are not good practices for development

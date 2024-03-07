5672d2359cd070ca2c8d4dd4f775fa775d9d4dd6
Abdul15de <126737630+Abdul15de@users.noreply.github.com>
2024-03-07 03:26:27 +0000

    strings and methods

---
+++ .devcontainer/strings.js
@@ -0,0 +1,27 @@
+// **** DECLAARTIONS OF STRINGS *****
+
+// strings can be decalred using tow ways
+
+// 1st
+let str = "Abdullah";
+console.log(str); //Abdullah 
+
+
+
+
+// 2nd
+let str2 = new String("abd");
+console.log(str2); //
+//String {'abd'}
+/* OUTPUT > Its output is in key:value pair as  object
+//String {'abd'}
+ 0 : "a"
+ 1 : "b"
+ 2 : "d"
+length : 3 */
+
+console.log(str[0]);//A
+console.log(str2[0]);//a 
+
+console.log(typeof str);//string
+console.log(typeof str2); //object
